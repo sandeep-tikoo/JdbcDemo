04f4eae2914ee3aaf82f0df355b3dc909db3ad79
Sandeep Tikoo <sandeep.tikoo@gmail.com>
2018-08-27 21:57:35 +0800

    Insert

--- src/main/java/org/arnav/javabrains/jdbc/dao/jdbcDaoImpl.java
+++ src/main/java/org/arnav/javabrains/jdbc/dao/jdbcDaoImpl.java
@@ -17,7 +17,7 @@ public class jdbcDaoImpl {
 	
 	
 	private DataSource dataSource;
-	private JdbcTemplate jdbcTemplate = new JdbcTemplate();
+	private JdbcTemplate jdbcTemplate;
 	
 	public int getTxnCount()	{
 		String sql = "select count(*) from bw_txn";
@@ -34,6 +34,26 @@ public class jdbcDaoImpl {
 	public List<bwTxnsModel> getTxnList()	{
 		String sql = "select * from bw_txn";
 		return jdbcTemplate.query(sql, new BwTxnsMapper());
+	}
+	
+	public void insertBwTxn(bwTxnsModel nR)	{
+		System.out.println("Befor SQL prepare");
+		String sql = "INSERT INTO bw_txn (bw_txn_id, BSB_Number, Account_Number, Transaction_Date, Narration, Cheque_Number, "
+				+ "Debit, Credit, Balance, Transaction_Type) "
+				+ " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		System.out.println("After SQL prepare");
+		jdbcTemplate.update(sql, new Object[] {nR.getTxnId()
+												,nR.getBSBNumber()
+												,nR.getAccountNumber()
+												,nR.getTxnDate()
+												,nR.getNarration()
+												,nR.getChqNo()
+												,nR.getDebitAmount()
+												,nR.getCreditAmount()
+												,nR.getBalance()
+												,nR.getTxnType()}
+							);
+		System.out.println("Befor SQL Execute");
 	}
 	
 	public DataSource getDataSource() {
--- src/main/java/org/arnav/javabrains/jdbc/main/JdbcDemo.java
+++ src/main/java/org/arnav/javabrains/jdbc/main/JdbcDemo.java
@@ -16,12 +16,12 @@ public class JdbcDemo {
 		System.out.println("Count from getTxnCount method: " + dao.getTxnCount());
 		
 		//Get row by ID
-		bwTxnsModel newTxn1 = dao.getTxn(2);
-		System.out.println("An instance for get ID method: " + newTxn1.getTxnId() + " <:> " + newTxn1.getBalance());
+		bwTxnsModel newTxn1 = dao.getTxn(998);
+		System.out.println("An instance for get ID method: " + newTxn1.getTxnId() + " <:> " + newTxn1.getBalance() + " <:> " + newTxn1.getNarration());
 
 		System.out.println("Size from get list method array: " + dao.getTxnList().size());
 
-		
+		//dao.insertBwTxn(new bwTxnsModel(998, "xxx-xxx",242216,"27/08/2018","xx yy zz","12345",100,99,1000,"TEST"));
 	}
 
 }
